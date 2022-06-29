8625152dbc605eb96a27faf41e736946a9b01f2a
poly000 <1348292515@qq.com>
2022-06-29 21:02:28 +0800

    update

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+const N: usize = 3;
+
+pub mod weapon {
+    use crate::N;
+    pub const MON_THU: [&'static str; N] = ["孤云寒林", "高塔孤王", "远海夷地"];
+    pub const TUE_FRI: [&'static str; N] = ["凛风奔狼", "雾海云间", "鸣神御灵"];
+    pub const WED_SAT: [&'static str; N] = ["狮牙斗士", "漆黑陨铁", "今昔剧画"];
+}
+
+pub mod talent {
+    use crate::N;
+    pub const MON_THU: [&'static str; N] = ["自由", "繁荣","浮世"];
+    pub const TUE_FRI: [&'static str; N] = ["抗争", "勤劳", "风雅"];
+    pub const WED_SAT: [&'static str; N] = ["诗文", "黄金", "天光"];
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,86 @@
-use chrono::{Datelike, Timelike, Weekday};
+use teloxide::{
+    prelude::*,
+    types::{
+        InlineQueryResult, InlineQueryResultArticle, InputMessageContent, InputMessageContentText,
+    },
+};
 
-fn now() -> (Weekday, u32) {
-    let time = chrono::Utc::now();
-    let weekday = time.date().weekday();
-    let hour = time.hour();
-    return (weekday, hour)
+use chrono::{Datelike, Timelike, Weekday};
+use daily_material::{talent, weapon};
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    let bot = Bot::from_env().auto_send();
+    let handler = Update::filter_inline_query().branch(dptree::endpoint(
+        |query: InlineQuery, bot: AutoSend<Bot>| async move {
+            let time = get_weekday().await;
+            let talent = get_talent(time).await;
+            let weapon = get_weapon(time).await;
+
+            let content_text = InputMessageContentText::new("欢迎使用这个bot！");
+            let content = InputMessageContent::Text(content_text);
+
+            let talent_text = InlineQueryResult::Article(InlineQueryResultArticle::new("天赋", talent, content.clone()));
+            let weapon_text = InlineQueryResult::Article(InlineQueryResultArticle::new("天赋", weapon, content));
+            let result = vec![talent_text, weapon_text];
+
+            let response = bot
+                .answer_inline_query(&query.id, result)
+                .cache_time(0)
+                .send()
+                .await;
+
+            if let Err(e) = response {
+                log::error!("Error in handler: {:?}", e);
+            }
+
+            respond(())
+        },
+    ));
+
+    Dispatcher::builder(bot, handler)
+        .build()
+        .setup_ctrlc_handler()
+        .dispatch()
+        .await;
 }
 
-fn main() {}
+async fn get_talent(time: Weekday) -> String {
+    let list = match time {
+        Weekday::Mon | Weekday::Thu => talent::MON_THU,
+        Weekday::Tue | Weekday::Fri => talent::TUE_FRI,
+        Weekday::Wed | Weekday::Sat => talent::WED_SAT,
+        _ => return "周日随便刷！".to_owned(),
+    };
+    format!("天赋材料：{}", list.join(" "))
+}
+
+async fn get_weapon(time: Weekday) -> String {
+    let list = match time {
+        Weekday::Mon | Weekday::Thu => weapon::MON_THU,
+        Weekday::Tue | Weekday::Fri => weapon::TUE_FRI,
+        Weekday::Wed | Weekday::Sat => weapon::WED_SAT,
+        _ => return "周日随便刷！".to_owned(),
+    };
+    format!("武器材料：{}", list.join(" "))
+}
+
+async fn get_weekday() -> Weekday {
+    let time = now().await;
+    let (weekday, hour) = time;
+    if hour >= 20 {
+        weekday.succ()
+    } else {
+        weekday
+    }
+}
+
+async fn now() -> (Weekday, u32) {
+    let time = chrono::Utc::now();
+
+    let weekday = time.date().weekday();
+    let hour = time.hour();
+
+    (weekday, hour)
+}
